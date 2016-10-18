d03f92a922e0738e87b41be261a8e44324adf5ff
JaneLawson <jane_lawson04@hotmail.com>
2016-10-18 13:57:36 +0100

    Initial commit

---
+++ src/TimesTablesApp/tt.js
@@ -0,0 +1,183 @@
+﻿/*
+*** TODO
+
+stop page from refreshing so doesn't start from scratch, and can't skip questions
+
+*/
+
+var firstNumber = $("#firstNumber");
+var secondNumber = $("#secondNumber");
+var givenAnswer = $("#givenAnswer");
+var okBtn = $("#okBtn");
+var nextBtn = $("#nextBtn");
+var errorMessage = $("#errorMessage");
+var correctAnswerText;
+var isCorrectAnswer = false;
+var startBtn = $("#startBtn");
+var tablesChosen = [];
+var selectAll = $("#selectAll");
+var allChecked = false;
+var quizSection = $("#quiz");
+var questionSection = $("#question");
+var cookie = document.cookie;
+
+cookie = "username=Jane Lawson; expires=Thu, 18 Oct 2016 00:00:00 UTC";
+
+(function () {
+    HideQuestionSection();
+})();
+
+function HideQuestionSection() {
+    questionSection.hide();
+}
+
+$("#choiceOfTables label").on("click", function () {
+    var $this = $(this);
+    if (this.htmlFor != "selectAll") {
+        if (isAlreadyInList(this)) {
+            $this.removeClass("chosen");
+        } else {
+            $this.addClass("chosen");
+            AddToList(this);
+        }
+    }
+});
+
+function startPracticing() {
+    startBtn.hide();
+    questionSection.show();
+    newQuestion();
+}
+
+selectAll.on("click", function () {
+    if (allChecked === false) {
+        window.tablesChosen = [];
+        $("#choiceOfTables label").each(function () {
+            if (this.htmlFor != "selectAll") {
+                $(this).addClass("chosen");
+                AddToList(this);
+            }
+        });
+        allChecked = true;
+    } else {
+        $("#choiceOfTables label").each(function () {
+            if (this.htmlFor != "selectAll") {
+                window.tablesChosen.splice(this, 1);
+                $(this).removeClass("chosen");
+            }
+        });
+        allChecked = false;
+    }
+});
+
+function AddToList(buttonClicked) {
+    var numToAdd = buttonClicked.innerText;
+    window.tablesChosen.push(parseInt(numToAdd));
+}
+
+function isAlreadyInList(buttonClicked) {
+    for (var i = 0; i < window.tablesChosen.length; i++) {
+        var $this = window.tablesChosen[i];
+        if ($this === parseInt(buttonClicked.innerText)) {
+            window.tablesChosen.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+function newQuestion() {
+    givenAnswer.prop("readonly", false);
+    setUpForNewInput();
+    var num1 = GetNumber1();
+    var num2 = parseInt(Math.random() * 12 + 1);
+    document.cookie = "numberTwo=" + num2 + ";";
+    firstNumber.text(num1);
+    secondNumber.text(num2);
+    correctAnswerText = num1 * num2;
+}
+
+function GetNumber1() {
+    var num1;
+    if (window.tablesChosen.length > 0) {
+        var randomItem = window.tablesChosen[Math.floor(Math.random() * window.tablesChosen.length)];
+        num1 = parseInt(randomItem);
+    } else {
+        num1 = parseInt(Math.random() * 12 + 1);
+    }
+    document.cookie = "numberOne=" + num1 + ";";
+    return num1;
+}
+
+function validateAnswer() {
+    errorMessage.show();
+    errorMessage.text("");
+    var answer = givenAnswer.val();
+    if (isNaN(answer)) {
+        try {
+            if (answer === "") {
+                throw new Error("Please type in your answer");
+            }
+            if (isNaN(parseInt(answer))) {
+                throw new Error("Sorry that isn't a number");
+            }
+        } catch (e) {
+            errorMessage.text(e.message);
+        }
+    }
+    else {
+        if (parseInt(answer) === correctAnswerText) {
+            isCorrectAnswer = true;
+            errorMessage.append("Well done its correct");
+            givenAnswer.prop("readonly", true);
+            SetUpNextButton();
+        } else {
+            errorMessage.append("Sorry thats not right");
+        }
+    }
+}
+
+function ShouldSetUpNewQuestion() {
+    if (isCorrectAnswer === false) {
+        setUpForNewInput();
+    }
+}
+
+function setUpForNewInput() {
+    isCorrectAnswer = false;
+    givenAnswer.val("");
+    givenAnswer.prop("readonly", false);
+    givenAnswer.focus();
+    SetUpOkButton();
+    errorMessage.text("");
+}
+
+function pressedEnter(e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code === 13) {
+        validateAnswer();
+    }
+}
+
+function SetUpOkButton() {
+    nextBtn.prop("disabled", true);
+    nextBtn.hide();
+    okBtn.prop("disabled", false);
+    okBtn.show();
+}
+
+function SetUpNextButton() {
+    nextBtn.prop("disabled", false);
+    nextBtn.show();
+    nextBtn.focus();
+    okBtn.prop("disabled", true);
+    okBtn.hide();
+}
+
+okBtn.click(validateAnswer);
+nextBtn.click(newQuestion);
+givenAnswer.keypress(pressedEnter);
+givenAnswer.click(ShouldSetUpNewQuestion);
+
+
+
