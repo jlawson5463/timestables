ad478ef2cdfe3029a5f20e149088cfaabf9c86fc
Jane-PC\Jane <jane_lawson04@hotmail.com>
2017-04-05 20:33:25 +0100

    changed routing to not include controller as too verbose

--- timestables/TimesTables/Timest4blesMVC/App_Start/RouteConfig.cs
+++ timestables/TimesTables/Timest4blesMVC/App_Start/RouteConfig.cs
@@ -13,11 +13,16 @@ namespace Timest4blesMVC
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               name:  "Default",
+               url: "{action}",
+               defaults: new { controller = "Home", action = "Index" });
         }
     }
 }
